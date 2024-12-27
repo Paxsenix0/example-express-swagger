c7f6b15f70ddeaba92802c8f59902658b1a78d9d
Alex <a24dzn@gmail.com>
2024-12-27 19:58:30 +0800

    Deleted more lines.
    Actually i just want to create a new deployment.

--- config/db.config.js
+++ config/db.config.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config();
-
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
