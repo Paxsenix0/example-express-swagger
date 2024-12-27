403fdc0bce2b4f6df6089249c74109abd2ba7a71
Alex <a24dzn@gmail.com>
2024-12-27 19:46:36 +0800

    Fixed invalid require path

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const rateLimiter = require('./middlewares/rateLimit.middleware.js');
-const connectDB = require('./src/config/db.config.js');
+const connectDB = require('./config/db.config.js');
 /* Connect to database */
 connectDB();
 
