8ac4d2113abedeca73c5cc7682037b4c388285ba
Alex <a24dzn@gmail.com>
2024-12-27 21:23:22 +0800

    Update chat.route.js

--- routes/chat.route.js
+++ routes/chat.route.js
@@ -1,8 +1,79 @@
-/*const express = require('express');
-const { model: novaModel, data: novaAI } = require('../plugins/nova-ai.js');
+const express = require('express');
+const { getChatModelHandler, getAvailableChatModels } = require('../plugins/index.js');;
 
 const chatRoutes = express.Router();
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     AIResponse:
+ *       type: object
+ *       properties:
+ *         message:
+ *           type: string
+ *           description: Additional message for the user
+ *         ok:
+ *           type: boolean
+ *           description: Indicates if the operation was successful
+ *       example:
+ *         message: "Hello! How can i assist you tomorrow?"
+ *         ok: true
+ * 
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   name: AI
+ *   description: Artificial intelligence endpoint to interact with AI, like chatting.
+ */
+
+/**
+ * @swagger
+ * /v1/chat/completions:
+ *   post:
+ *     summary: Nova-AI
+ *     tags: [AI]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               model:
+ *                 type: string
+ *                 description: The model of the chat AI, leave it empty to see available models
+ *               messages:
+ *                 type: array
+ *                 items:
+ *                   type: object
+ *                   properties:
+ *                     role:
+ *                       type: string
+ *                       enum: [user, assistant, system]
+ *                       description: The role of the entity in the conversation
+ *                     content:
+ *                       type: string
+ *                       description: The actual message content
+ *           example:
+ *             model: "nova-ai"
+ *             messages:
+ *               - role: user
+ *                 content: "Hello"
+ *     responses:
+ *       200:
+ *         description: Success - Got the response
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/AIResponse'
+ *       400:
+ *         description: Invalid input
+ *       500:
+ *         description: Server error
+ */
 chatRoutes.post('/v1/chat/completions', async (req, res) => {
   const body = req.body;
 
@@ -10,12 +81,19 @@ chatRoutes.post('/v1/chat/completions', async (req, res) => {
     return res.status(400).json({ message: 'invalid request body', ok: false });
   }
 
-  if (!body.model || body.model.includes([novaModel])) {
-    return res.status(400).json({ message: 'invalid model! see available models', models: [novaModel], ok: false });
+  const { model } = body;
+  const handleMessage = getChatModelHandler(model);
+
+  if (!handleMessage) {
+    return res.status(400).json({
+      message: 'invalid model! see available models',
+      models: getAvailableChatModels(),
+      ok: false,
+    });
   }
 
   try {
-    const response = await novaAI(body);
+    const response = await handleMessage(body);
     if (!response) {
       return res.status(500).json({ message: 'failed to get a response from the AI', ok: false });
     }
@@ -26,4 +104,4 @@ chatRoutes.post('/v1/chat/completions', async (req, res) => {
   }
 });
 
-module.exports = chatRoutes;*/
+module.exports = chatRoutes;
