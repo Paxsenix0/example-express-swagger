46c5d0b7cfbabc7650e7cb6c67a7fabc1eb7090a
Alex <a24dzn@gmail.com>
2024-12-27 20:05:32 +0800

    Added missing an option "next"

--- middlewares/rateLimit.middleware.js
+++ middlewares/rateLimit.middleware.js
@@ -1,10 +1,10 @@
 const ipAddressService = require('../services/ip.service.js');
 
-const rateLimiter = async (req, res) => {
-    const ip = req.ip; // or however u get the client ip
+const rateLimiter = async (req, res, next) => {
+    const ip = req.ip;
     const isAllowed = await ipAddressService.checkUsage(ip);
     if (!isAllowed) {
-        return res.status(429).json({ message: 'too many requests', ok: false });
+        return res.status(429).json({ message: 'Too Many requests, genius.', ok: false });
     }
     await ipAddressService.addUsage(ip);
     next();
