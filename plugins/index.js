83d74e9d57ca70e2a12c98a4a1d201081cddd92f
Alex <a24dzn@gmail.com>
2024-12-27 23:45:15 +0800

    Renaming models to chatModels for easier to organize

--- plugins/index.js
+++ plugins/index.js
@@ -1,13 +1,13 @@
 const novaAI = require('./nova-ai.js');
 
-const models = [novaAI];
+const chatModels = [novaAI];
 
 function getChatModelHandler(modelName) {
-  return models.find((m) => m.model === modelName)?.handleMessage || null;
+  return chatModels.find((m) => m.model === modelName)?.handleMessage || null;
 }
 
 function getAvailableChatModels() {
-  return models.map((m) => m.model);
+  return chatModels.map((m) => m.model);
 }
 
 module.exports = { getChatModelHandler, getAvailableChatModels };
