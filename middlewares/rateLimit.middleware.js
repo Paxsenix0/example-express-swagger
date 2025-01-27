549059c167362a47fac8de0ab51e75569ccbd534
Alex <a24dzn@gmail.com>
2025-01-27 14:42:53 +0800

    Somehow it doesn't get ip address, why tho???

--- middlewares/rateLimit.middleware.js
+++ middlewares/rateLimit.middleware.js
@@ -1,7 +1,7 @@
 const ipAddressService = require('../services/ip.service.js');
 
 const rateLimiter = async (req, res, next) => {
-    const ip = req.ip;
+    const ip = await getIpFromRequest(req);
     const isAllowed = await ipAddressService.checkUsage(ip);
     if (!isAllowed) {
         return res.status(429).json({ message: 'Too Many requests, genius.', ok: false });
@@ -9,5 +9,14 @@ const rateLimiter = async (req, res, next) => {
     await ipAddressService.addUsage(ip);
     next();
 };
+
+async function getIpFromRequest(request) {
+    let ip = request.ip ?? request.headers["x-real-ip"];
+    if (!ip || ip === '127.0.0.1') {
+      const forwardedFor = request.headers["x-forwarded-for"];
+      ip = forwardedFor.split(",").at(0) ?? null;
+    }
+    return ip;
+}
 
 module.exports = rateLimiter;
