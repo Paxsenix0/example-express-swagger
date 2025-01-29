8e3880e6790ffb980d872c7e8c288e846635ac81
Alex <a24dzn@gmail.com>
2025-01-29 21:34:30 +0800

    Added it to handler

--- plugins/index.js
+++ plugins/index.js
@@ -1,10 +1,11 @@
 const novaAI = require('./nova-ai.js');
 const gpt4omini = require('./gpt4omini.js');
+const gpt3 = require('./gpt3.js');
 const gemini = require('./gemini.js');
 const sdxl = require('./startnest.js');
 const flux = require('./genius.js');
 
-const chatModels = [novaAI, gpt4omini, gemini];
+const chatModels = [novaAI, gpt4omini, gemini, gpt3];
 const imageModels = [sdxl, flux];
 
 function getChatModelHandler(modelName) {
